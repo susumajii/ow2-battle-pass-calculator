b1856f67b369370fe7f7cb5b8eddff2dafaced35
susumajii <56935411+susumajii@users.noreply.github.com>
2025-12-09 21:48:38 -0600

    Update seasons.js
    Added season 20: Vendetta with start date 12/9 and end date 2/9

--- seasons.js
+++ seasons.js
@@ -2,11 +2,15 @@
 // Keep newest season at the TOP of the array.
 window.SEASONS = [
   {
+    season: 20,
+    name: "Vendetta",
+    start: "2025-12-09T18:00:00Z",
+    end:   "2026-02-09T18:00:00Z"
+  },
+  {
     season: 19,
     name: "Haunted Masquerade",
     start: "2025-10-14T18:00:00Z",
     end:   "2025-12-09T18:00:00Z"
-  },
-  // When Season 20 launches, add it ABOVE this one, e.g.:
-  // { season: 20, name: "SEASON 20 NAME", start: "YYYY-MM-DDTHH:mm:ssZ", end: "YYYY-MM-DDTHH:mm:ssZ" },
+  }
 ];
