992fae5deb68dbb797fd47a9c737637f599b5a1c
susumajii <56935411+susumajii@users.noreply.github.com>
2026-02-14 14:36:16 -0600

    Update seasons.js for latest season

--- seasons.js
+++ seasons.js
@@ -2,6 +2,12 @@
 // Keep newest season at the TOP of the array.
 window.SEASONS = [
   {
+    season: 1,
+    name: "Conquest — The Reign of Talon",
+    start: "2026-02-10T18:00:00Z",
+    end:   "2026-04-14T18:00:00Z"
+  },
+  {
     season: 20,
     name: "Vendetta",
     start: "2025-12-09T18:00:00Z",
